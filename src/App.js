812a0b8c8641e0df39e88b045a992a76552e97f6
Lawrence <locaulker@gmail.com>
2020-01-07 07:03:42 -0500

    end of 29

--- src/App.js
+++ src/App.js
@@ -1,51 +1,14 @@
 import React, { Component } from 'react'
-// import logo from './logo.svg';
 import './App.css'
-import Table from './components/Table'
-// import FragmentDemo from './components/FragmentDemo'
-// import Greet from './components/Greet'
-// import Welcome from './components/Welcome'
-// import Hello from './components/Hello'
-// import Message from './components/Message'
-// import Counter from './components/Counter'
-// import FunctionClick from './components/FunctionClick'
-// import ClassClick from './components/ClassClick'
-// import EventBind from './components/EventBind'
-// import ParentComponent from './components/ParentComponent'
-// import UserGreeting from './components/UserGreeting'
-// import NameList from './components/NameList'
-// import Stylesheet from './components/Stylesheet'
-// import Inline from './components/Inline'
-// import Form from './components/Form'
-// import LifecycleA from './components/LifecycleA'
+import FocusInput from './components/FocusInput'
+// import RefsDemo from './components/RefsDemo'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Counter /> */}
-        {/* <Message /> */}
-        {/* <Greet name="Bruce" heroName="Batman"><p>Children props go here</p></Greet> */}
-        {/* <Greet name="Clark" heroName="Superman"><button>Action</button></Greet> */}
-        {/* <Greet name="Diana" heroName="Wonder Woman" /> */}
-        {/* <hr/> */}
-        {/* <Welcome name="Bruce" heroName="Batman" /> */}
-        {/* <Welcome name="Clark" heroName="Superman" /> */}
-        {/* <Welcome name="Diana" heroName="Wonder Woman" /> */}
-        {/* <hr/> */}
-        {/* <Hello /> */}
-        {/* <FunctionClick /> */}
-        {/* <ClassClick /> */}
-        {/* <EventBind /> */}
-        {/* <ParentComponent /> */}
-        {/* <UserGreeting /> */}
-        {/* <NameList /> */}
-        {/* <Stylesheet primary={true} /> */}
-        {/* <Inline /> */}
-        {/* <Form /> */}
-        {/* <LifecycleA /> */}
-        {/* <FragmentDemo /> */}
-        <Table />
+        {/* <RefsDemo /> */}
+        <FocusInput />
       </div>
     );
   }
